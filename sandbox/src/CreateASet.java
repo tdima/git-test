dd1f33078a8c098f8513831e862add430671dea5
Dima <dmitry.tumash@db.com>
2014-11-13 22:07:52 +0300

    check code style

--- sandbox/src/CreateASet.java
+++ sandbox/src/CreateASet.java
@@ -8,17 +8,17 @@ import java.util.Arrays;
  */
 public class CreateASet {
 
-  public static void main(String[] args) {
-    String[] words = {"A", "B", "B", "D", "C", "A"};
-    System.out.println("original: " + Arrays.toString(words));
-    System.out.println("as a set: ");
-    Rectangle[] rectList = {new Rectangle(), new Rectangle(),
-            new Rectangle(0, 1, 2, 3), new Rectangle(0, 1, 2, 3)};
-    System.out.println("original: " + Arrays.toString(rectList));
-    System.out.println("as a set: ");
-    Object[] mixed = {"A", "C", "A", "B", new Rectangle(),
-            new Rectangle(), "A", new Rectangle(0, 1, 2, 3), "D"};
-    System.out.println("original: " + Arrays.toString(mixed));
-    System.out.println("as a set: ");
-  }
+    public static void main(String[] args) {
+        String[] words = {"A", "B", "B", "D", "C", "A"};
+        System.out.println("original: " + Arrays.toString(words));
+        System.out.println("as a set: ");
+        Rectangle[] rectList = {new Rectangle(), new Rectangle(),
+                new Rectangle(0, 1, 2, 3), new Rectangle(0, 1, 2, 3)};
+        System.out.println("original: " + Arrays.toString(rectList));
+        System.out.println("as a set: ");
+        Object[] mixed = {"A", "C", "A", "B", new Rectangle(),
+                new Rectangle(), "A", new Rectangle(0, 1, 2, 3), "D"};
+        System.out.println("original: " + Arrays.toString(mixed));
+        System.out.println("as a set: ");
+    }
 }
